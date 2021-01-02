5ea0bdaa13117d152ce01421ca8e5a44d36003a2
Iker <lewricka@gmail.com>
2021-01-02 15:42:20 +0100

    [] - Improve images

--- src/components/climbing_plan/Slider.js
+++ src/components/climbing_plan/Slider.js
@@ -23,7 +23,7 @@ export default function Slider() {
             edges {
               node {
                 title
-                fluid(quality:100) {
+                fluid(quality:100, maxWidth: 800) {
                   ...GatsbyContentfulFluid_withWebp
                 }
               }
